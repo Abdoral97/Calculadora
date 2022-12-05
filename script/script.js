8425b0c6cc23724cd62c2b7b16ce61c362ced420
Daniel Abdoral <115808976+Abdoral97@users.noreply.github.com>
2022-12-05 17:10:41 -0300

    Atualizando o projeto

---
+++ script/script.js
@@ -0,0 +1,27 @@
+/*NÚMEROS-BOTÃO*/
+const b0 = document.getElementById("butao-zero")
+const b1 = document.getElementById("butao-um")
+const b2 = document.getElementById("butao-dois")
+const b3 = document.getElementById("butao-tres")
+const b4 = document.getElementById("butao-quatro")
+const b5 = document.getElementById("butao-cinco")
+const b6 = document.getElementById("butao-seis")
+const b7 = document.getElementById("butao-sete")
+const b8 = document.getElementById("butao-oito")
+const b9 = document.getElementById("butao-nove")
+const bvirg = document.getElementById("butao-virg")
+
+/*OPERADORES e OPÇÕES*/
+
+const bsoma = document.getElementById("butao-adic")
+const bsub = document.getElementById("butao-sub")
+const bdiv = document.getElementById("butao-divi")
+const bmulti = document.getElementById("butao-multi")
+const bresul = document.getElementById("butao-resul")
+const bc = document.getElementById("butao-c")
+const bce = document.getElementById("butao-ce")
+
+
+
+
+
